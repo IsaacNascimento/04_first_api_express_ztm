e6c7c7a8748dc66070e1ad3e99ed62c565c6d61e
Isaac Nascimento <isaacs.nascimento17@gmail.com>
2023-02-01 17:14:16 -0300

    nodemon watch server. Post Friend. Middlewares app.use

--- server.js
+++ server.js
@@ -3,10 +3,38 @@ const app = express();
 const PORT = 4000;
 
 const friends = [
-  { id: 0, name: "Sir Isaac Newton" },
+  { id: 0, name: "Isaac Newton" },
   { id: 1, name: "Albert Einstein" },
   { id: 2, name: "Nikolas Tesla" },
 ];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const end = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${end}ms`);
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Welcome to Friend´s API!");
+});
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "The name field is required",
+    });
+  }
+  const newFriend = {
+    name: req.body?.name,
+    id: friends.length,
+  };
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
 
 app.get("/friends", (req, res) => {
   res.json(friends);
