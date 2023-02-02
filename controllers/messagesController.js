e658a469be9394bb2e1b198a1ba240654251795f
Isaac Nascimento <isaacs.nascimento17@gmail.com>
2023-02-02 10:08:05 -0300

    layout template. Basic template be render. Message controller returning handlebar file.

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,14 +1,18 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  const fileToSend = path.join(
-    __dirname,
-    "..",
-    "public",
-    "images",
-    "skimountain.jpg"
-  );
-  res.sendFile(fileToSend);
+  // const fileToSend = path.join(
+  //   __dirname,
+  //   "..",
+  //   "public",
+  //   "images",
+  //   "skimountain.jpg"
+  // );
+  // res.sendFile(fileToSend);
+  res.render("messages", {
+    title: "Messages to My Friends",
+    friend: "Stephen Hawking",
+  });
 }
 
 function postMessage(req, res) {
